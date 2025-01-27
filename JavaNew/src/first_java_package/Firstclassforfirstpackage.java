8f6ace4c54f1f011396164749150ac79ce423c1c
alish <alish@192.168.10.44>
2025-01-27 18:51:35 +0500

    1 PC to another

---
+++ JavaNew/src/first_java_package/Firstclassforfirstpackage.java
@@ -0,0 +1,13 @@
+package first_java_package;
+
+public class Firstclassforfirstpackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello!");		
+		System.out.print("hello!"); //my new code
+
+	}
+
+}
