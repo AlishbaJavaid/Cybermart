923690a27594cc7e30f9b589be9fc27cc26afcc5
alish <alish@192.168.10.44>
2025-01-27 20:53:54 +0500

    2nd commit

---
+++ JavaNew/src/first_java_package/ExceptionHandlingDemo.java
@@ -0,0 +1,12 @@
+package first_java_package;
+
+public class ExceptionHandlingDemo {
+	
+	public static void main(String[] args) {
+			
+			System.out.println("hello...!");
+			System.out.println("New Message!");
+			
+		}
+		
+}
--- JavaNew/src/first_java_package/Firstclassforfirstpackage.java
+++ JavaNew/src/first_java_package/Firstclassforfirstpackage.java
@@ -5,7 +5,7 @@ public class Firstclassforfirstpackage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello!");		
+		System.out.println("hello...!");		
 		System.out.print("hello!"); //my new code
 
 	}
